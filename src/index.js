83f7a0691d62f33e418dbceb3322b3e5b4f7f5df
Gautam kumar <gautamsiddharth2013@gmail.com>
2026-01-19 16:39:24 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 import { store } from "./app/store";
-import 'regenerator-runtime/runtime'
 import { Provider } from "react-redux";
 
 
